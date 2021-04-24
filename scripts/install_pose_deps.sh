2db57c29eb8025d09ac3a710b24719732d0eb993
tjysdsg <tjy1018543509@163.com>
2021-04-24 20:41:14 +0800

    Add scripts to install dependencies

--- scripts/install_pose_deps.sh
+++ scripts/install_pose_deps.sh
@@ -1,7 +1,14 @@
 ############ install openface ############
-pushd pose
+pushd pose/openface
 
 python setup.py install
 ./models/get-models.sh
+
+popd
+
+############ install openpose ############
+pushd pose/openpose
+
+pip install -r requirements.txt
 
 popd
