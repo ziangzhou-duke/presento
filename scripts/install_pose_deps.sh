dcf0238b99b9da0cf5d8cc18ecc486f5000f47b0
tjysdsg <tjy1018543509@163.com>
2021-04-24 20:55:31 +0800

    Remove openface its the wrong version

--- scripts/install_pose_deps.sh
+++ scripts/install_pose_deps.sh
@@ -1,11 +1,3 @@
-############ install openface ############
-pushd pose/openface
-
-python setup.py install
-./models/get-models.sh
-
-popd
-
 ############ install openpose ############
 pushd pose/openpose
 
