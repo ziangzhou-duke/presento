40a287d5c1834f5cdd5437a81bba553139b15190
tjysdsg <tjy1018543509@163.com>
2021-04-28 12:10:01 +0800

    Successfully run openpose camera demo

--- pose/openpose/demo_camera.py
+++ pose/openpose/demo_camera.py
@@ -1,4 +1,5 @@
 import cv2
+import os
 import matplotlib.pyplot as plt
 import copy
 import numpy as np
@@ -8,37 +9,43 @@ from src import model
 from src import util
 from src.body import Body
 from src.hand import Hand
+from pose import POSE_ROOT_DIR
 
-body_estimation = Body('model/body_pose_model.pth')
-hand_estimation = Hand('model/hand_pose_model.pth')
 
-print(f"Torch device: {torch.cuda.get_device_name()}")
+def main():
+    body_estimation = Body(os.path.join(POSE_ROOT_DIR, 'openpose', 'model', 'body_pose_model.pth'))
+    hand_estimation = Hand(os.path.join(POSE_ROOT_DIR, 'openpose', 'model', 'hand_pose_model.pth'))
 
-cap = cv2.VideoCapture(0)
-cap.set(3, 640)
-cap.set(4, 480)
-while True:
-    ret, oriImg = cap.read()
-    candidate, subset = body_estimation(oriImg)
-    canvas = copy.deepcopy(oriImg)
-    canvas = util.draw_bodypose(canvas, candidate, subset)
+    print(f"Torch device: {torch.cuda.get_device_name()}")
 
-    # detect hand
-    hands_list = util.handDetect(candidate, subset, oriImg)
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 640)
+    cap.set(4, 480)
+    while True:
+        ret, oriImg = cap.read()
+        candidate, subset = body_estimation(oriImg)
+        canvas = copy.deepcopy(oriImg)
+        canvas = util.draw_bodypose(canvas, candidate, subset)
 
-    all_hand_peaks = []
-    for x, y, w, is_left in hands_list:
-        peaks = hand_estimation(oriImg[y:y+w, x:x+w, :])
-        peaks[:, 0] = np.where(peaks[:, 0]==0, peaks[:, 0], peaks[:, 0]+x)
-        peaks[:, 1] = np.where(peaks[:, 1]==0, peaks[:, 1], peaks[:, 1]+y)
-        all_hand_peaks.append(peaks)
+        # detect hand
+        hands_list = util.handDetect(candidate, subset, oriImg)
 
-    canvas = util.draw_handpose(canvas, all_hand_peaks)
+        all_hand_peaks = []
+        for x, y, w, is_left in hands_list:
+            peaks = hand_estimation(oriImg[y:y + w, x:x + w, :])
+            peaks[:, 0] = np.where(peaks[:, 0] == 0, peaks[:, 0], peaks[:, 0] + x)
+            peaks[:, 1] = np.where(peaks[:, 1] == 0, peaks[:, 1], peaks[:, 1] + y)
+            all_hand_peaks.append(peaks)
 
-    cv2.imshow('demo', canvas)#一个窗口用以显示原视频
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        canvas = util.draw_handpose(canvas, all_hand_peaks)
 
-cap.release()
-cv2.destroyAllWindows()
+        cv2.imshow('demo', canvas)  # 一个窗口用以显示原视频
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
