e94852c5cae8406414bd42aafacdac790222c820
tjysdsg <tjy1018543509@163.com>
2021-04-28 14:18:43 +0800

    Fix and test keypoints_from_image.py

--- pose/openpose/openpose_python.py
+++ pose/openpose/openpose_python.py
@@ -1,32 +1,14 @@
 import sys
-import os
 import argparse
-from pose.config import OPENPOSE_INSTALL_DIR
+from pose.openpose.import_libs import openpose, openpose_model_folder
 
 try:
-    # Import Openpose (Windows/Ubuntu/OSX)
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    try:
-        # Change these variables to point to the correct folder (Release/x64 etc.)
-        sys.path.append(os.path.join(OPENPOSE_INSTALL_DIR, 'bin', 'python', 'openpose', 'Release'))
-        os.environ['PATH'] = os.environ['PATH'] + ';' + \
-                             os.path.join(OPENPOSE_INSTALL_DIR, 'x64', 'Release') + ';' + \
-                             os.path.join(OPENPOSE_INSTALL_DIR, 'bin') + ';'
-        import pyopenpose as op
-    except ImportError as e:
-        print(
-            'Error: OpenPose library could not be found. Did you enable `BUILD_PYTHON` in CMake and have this Python '
-            'script in the right folder?'
-        )
-        raise e
-
-    # Flags
     parser = argparse.ArgumentParser()
     args = parser.parse_known_args()
 
     # Custom Params (refer to include/openpose/flags.hpp for more parameters)
     params = dict()
-    params["model_folder"] = os.path.join(OPENPOSE_INSTALL_DIR, "models")
+    params["model_folder"] = openpose_model_folder
 
     # Add others in path?
     for i in range(0, len(args[1])):
@@ -49,7 +31,7 @@ try:
     # oppython = op.OpenposePython()
 
     # Starting OpenPose
-    opWrapper = op.WrapperPython(op.ThreadManagerMode.Synchronous)
+    opWrapper = openpose.WrapperPython(openpose.ThreadManagerMode.Synchronous)
     opWrapper.configure(params)
     opWrapper.execute()
 except Exception as e:
