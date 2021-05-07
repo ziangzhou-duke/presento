fa30b9a10e9bc248124d9d1fcf5692cf06c39c06
tjysdsg <tjy1018543509@163.com>
2021-05-07 11:59:49 +0800

    Output annotated video in pose infer script

--- pose/infer.py
+++ pose/infer.py
@@ -10,7 +10,7 @@ from pose.extract_skeleton import SkeletonExtractor
 from pose.config import NUM_FRAMES_PER_SEGMENT
 from pose.models import BodyFaceEmotionClassifier
 from pose.utils import visualize_skeleton_openpose
-from pose.datasets import BodyFaceDataset, normalize_skeleton
+from pose.datasets import BodyFaceDataset, normalize_skeleton, inv_babyrobot_mapper
 from torch.utils.data import DataLoader
 
 pose_extractor = SkeletonExtractor()
@@ -53,7 +53,7 @@ def infer_emotion(model: BodyFaceEmotionClassifier, poses: np.ndarray, left_hand
         )
         out = model((body, hand_right, hand_left, length))
         print(out)
-        return np.argmax(out.cpu().detach().numpy(), -1)
+        return np.argmax(out.cpu().detach().numpy(), -1)[0]
 
 
 def main():
@@ -62,11 +62,20 @@ def main():
     model.eval()
 
     vidcap = cv2.VideoCapture(args.input)
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out_vid = cv2.VideoWriter(
+        filename='out.mp4', fourcc=fourcc, fps=vidcap.get(cv2.CAP_PROP_FPS),
+        frameSize=(
+            int(vidcap.get(cv2.CAP_PROP_FRAME_WIDTH)),
+            int(vidcap.get(cv2.CAP_PROP_FRAME_HEIGHT)),
+        )
+    )
 
     poses = []
     left_hands = []
     right_hands = []
     success, image = vidcap.read()
+    emotion = 6
     count = 0
     while success:
         pose, left_hand, right_hand = get_first_person(image)
@@ -76,21 +85,48 @@ def main():
         left_hands.append(left_hand.ravel())
         right_hands.append(right_hand.ravel())
 
-        # if count >= NUM_FRAMES_PER_SEGMENT:
-        #     print(infer_emotion(
-        #         model, np.vstack(poses), np.vstack(left_hands), np.vstack(right_hands)
-        #     ))
-        #     poses = []
-        #     left_hands = []
-        #     right_hands = []
+        if count >= NUM_FRAMES_PER_SEGMENT:
+            emotion = infer_emotion(
+                model, np.vstack(poses), np.vstack(left_hands), np.vstack(right_hands)
+            )
+            poses = []
+            left_hands = []
+            right_hands = []
+
+        # draw label
+        label_size, base_line = cv2.getTextSize(
+            f"{inv_babyrobot_mapper[emotion]}", cv2.FONT_HERSHEY_SIMPLEX, 1.0, 2
+        )
+        startx = label_size[0]
+        starty = label_size[1]
+        cv2.rectangle(
+            image,
+            (startx, starty + 1 - label_size[1]),
+            (startx + label_size[0], starty + 1 + base_line),
+            (223, 128, 255),
+            cv2.FILLED,
+        )
+        cv2.putText(
+            image,
+            f"{inv_babyrobot_mapper[emotion]}",
+            (startx, starty + 1),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1.0,
+            (0, 0, 0),
+            2,
+        )
+        out_vid.write(image)
 
         success, image = vidcap.read()
         count += 1
 
-    print(infer_emotion(
-        model, np.vstack(poses), np.vstack(left_hands), np.vstack(right_hands)
-    ))
-    visualize_skeleton_openpose(poses[0].reshape(-1, 3), left_hands[0].reshape(-1, 3), right_hands[1].reshape(-1, 3))
+    # print(infer_emotion(
+    #     model, np.vstack(poses), np.vstack(left_hands), np.vstack(right_hands)
+    # ))
+
+    # visualize_skeleton_openpose(poses[0].reshape(-1, 3), left_hands[0].reshape(-1, 3), right_hands[1].reshape(-1, 3))
+
+    out_vid.release()
 
 
 if __name__ == '__main__':
