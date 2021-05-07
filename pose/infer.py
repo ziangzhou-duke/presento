62565d56981e476131d518cb318d6b8b7e7a2470
tjysdsg <tjy1018543509@163.com>
2021-05-07 12:47:01 +0800

    Refactor face and pose inference code for future ensemble

--- pose/infer.py
+++ pose/infer.py
@@ -8,58 +8,114 @@ import numpy as np
 import torch
 from pose.extract_skeleton import SkeletonExtractor
 from pose.config import NUM_FRAMES_PER_SEGMENT
+from pose import POSE_ROOT_DIR
 from pose.models import BodyFaceEmotionClassifier
 from pose.utils import visualize_skeleton_openpose
 from pose.datasets import BodyFaceDataset, normalize_skeleton, inv_babyrobot_mapper
 from torch.utils.data import DataLoader
 
-pose_extractor = SkeletonExtractor()
+
+class PoseEmotionEstimator:
+    def __init__(self):
+        self.poses = []
+        self.left_hands = []
+        self.right_hands = []
+        self.emotion = 6
+        self.model: BodyFaceEmotionClassifier = torch.load(os.path.join(POSE_ROOT_DIR, '8.ckpt')).cuda()
+        self.model.eval()
+        self.count = 0
+
+        self.pose_extractor = SkeletonExtractor()
+
+    def __call__(self, frame: np.ndarray) -> np.ndarray:
+        return self.process_frame(frame)
+
+    def process_frame(self, frame: np.ndarray) -> np.ndarray:
+        out_frame = np.zeros_like(frame)
+
+        pose, left_hand, right_hand = self.get_first_person(frame)
+        pose, left_hand, right_hand = normalize_skeleton(pose, left_hand, right_hand)
+
+        self.poses.append(pose.ravel())
+        self.left_hands.append(left_hand.ravel())
+        self.right_hands.append(right_hand.ravel())
+
+        if self.count >= NUM_FRAMES_PER_SEGMENT:
+            self.emotion = self.infer_emotion(
+                np.vstack(self.poses), np.vstack(self.left_hands), np.vstack(self.right_hands)
+            )
+            self.poses = []
+            self.left_hands = []
+            self.right_hands = []
+
+        # draw label
+        label_size, base_line = cv2.getTextSize(
+            f"{inv_babyrobot_mapper[self.emotion]}", cv2.FONT_HERSHEY_SIMPLEX, 1.0, 2
+        )
+        startx = label_size[0]
+        starty = label_size[1]
+        cv2.rectangle(
+            out_frame,
+            (startx, starty + 1 - label_size[1]),
+            (startx + label_size[0], starty + 1 + base_line),
+            (223, 128, 255),
+            cv2.FILLED,
+        )
+        cv2.putText(
+            out_frame,
+            f"{inv_babyrobot_mapper[self.emotion]}",
+            (startx, starty + 1),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1.0,
+            (0, 0, 0),
+            2,
+        )
+
+        self.count += 1
+        return out_frame
+
+    def infer_emotion(self, poses: np.ndarray, left_hands: np.ndarray, right_hands: np.ndarray):
+        n = left_hands.shape[0]
+        dataset = BodyFaceDataset(
+            [0], np.expand_dims(poses, 0), np.expand_dims(left_hands, 0), np.expand_dims(right_hands, 0), [n],
+            [0], [0]
+        )
+        dataset.set_scaler(self.model.scalers)
+        dataset.to_tensors()
+        dataset.prepad()
+        dataloader = DataLoader(
+            dataset, shuffle=True, batch_size=32, num_workers=4
+        )
+
+        for i, batch in enumerate(dataloader):  # should only have one iter
+            body, hand_right, hand_left, length = (
+                batch['body'].cuda(), batch['hand_right'].cuda(), batch['hand_left'].cuda(), torch.tensor([n]).cuda()
+            )
+            out = self.model((body, hand_right, hand_left, length))
+            print(out)
+            return np.argmax(out.cpu().detach().numpy(), -1)[0]
+
+    def get_first_person(self, image: np.ndarray):
+        pose, hands = self.pose_extractor.extract_skeletons(image)
+
+        # pose and hands of the first person
+        pose = pose[0]
+        left_hand = hands[0][0]
+        right_hand = hands[1][0]
+        return pose, left_hand, right_hand
 
 
 def parse_opts():
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('--input', type=str)
-    parser.add_argument('--model_path', type=str)
     args = parser.parse_args()
     return args
 
 
-def get_first_person(image: np.ndarray):
-    pose, hands = pose_extractor.extract_skeletons(image)
-
-    # pose and hands of the first person
-    pose = pose[0]
-    left_hand = hands[0][0]
-    right_hand = hands[1][0]
-    return pose, left_hand, right_hand
-
-
-def infer_emotion(model: BodyFaceEmotionClassifier, poses: np.ndarray, left_hands: np.ndarray, right_hands: np.ndarray):
-    n = left_hands.shape[0]
-    dataset = BodyFaceDataset(
-        [0], np.expand_dims(poses, 0), np.expand_dims(left_hands, 0), np.expand_dims(right_hands, 0), [n],
-        [0], [0]
-    )
-    dataset.set_scaler(model.scalers)
-    dataset.to_tensors()
-    dataset.prepad()
-    dataloader = DataLoader(
-        dataset, shuffle=True, batch_size=32, num_workers=4
-    )
-
-    for i, batch in enumerate(dataloader):  # should only have one iter
-        body, hand_right, hand_left, length = (
-            batch['body'].cuda(), batch['hand_right'].cuda(), batch['hand_left'].cuda(), torch.tensor([n]).cuda()
-        )
-        out = model((body, hand_right, hand_left, length))
-        print(out)
-        return np.argmax(out.cpu().detach().numpy(), -1)[0]
-
-
 def main():
     args = parse_opts()
-    model = torch.load(args.model_path).cuda()
-    model.eval()
+
+    estimator = PoseEmotionEstimator()
 
     vidcap = cv2.VideoCapture(args.input)
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
@@ -71,58 +127,12 @@ def main():
         )
     )
 
-    poses = []
-    left_hands = []
-    right_hands = []
-    success, image = vidcap.read()
-    emotion = 6
-    count = 0
+    success, frame = vidcap.read()
     while success:
-        pose, left_hand, right_hand = get_first_person(image)
-        pose, left_hand, right_hand = normalize_skeleton(pose, left_hand, right_hand)
+        frame = estimator(frame)
 
-        poses.append(pose.ravel())
-        left_hands.append(left_hand.ravel())
-        right_hands.append(right_hand.ravel())
-
-        if count >= NUM_FRAMES_PER_SEGMENT:
-            emotion = infer_emotion(
-                model, np.vstack(poses), np.vstack(left_hands), np.vstack(right_hands)
-            )
-            poses = []
-            left_hands = []
-            right_hands = []
-
-        # draw label
-        label_size, base_line = cv2.getTextSize(
-            f"{inv_babyrobot_mapper[emotion]}", cv2.FONT_HERSHEY_SIMPLEX, 1.0, 2
-        )
-        startx = label_size[0]
-        starty = label_size[1]
-        cv2.rectangle(
-            image,
-            (startx, starty + 1 - label_size[1]),
-            (startx + label_size[0], starty + 1 + base_line),
-            (223, 128, 255),
-            cv2.FILLED,
-        )
-        cv2.putText(
-            image,
-            f"{inv_babyrobot_mapper[emotion]}",
-            (startx, starty + 1),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            1.0,
-            (0, 0, 0),
-            2,
-        )
-        out_vid.write(image)
-
-        success, image = vidcap.read()
-        count += 1
-
-    # print(infer_emotion(
-    #     model, np.vstack(poses), np.vstack(left_hands), np.vstack(right_hands)
-    # ))
+        out_vid.write(frame)
+        success, frame = vidcap.read()
 
     # visualize_skeleton_openpose(poses[0].reshape(-1, 3), left_hands[0].reshape(-1, 3), right_hands[1].reshape(-1, 3))
 
--- realtime_face/ssd_infer.py
+++ realtime_face/ssd_infer.py
@@ -8,6 +8,113 @@ from realtime_face.models.resmasking import resmasking_dropout1
 from realtime_face import FACE_ROOT_DIR
 
 
+class FaceEmotionEstimator:
+    def __init__(self):
+        self.net = cv2.dnn.readNetFromCaffe(
+            os.path.join(FACE_ROOT_DIR, "deploy.prototxt.txt"),
+            os.path.join(FACE_ROOT_DIR, "res10_300x300_ssd_iter_140000.caffemodel")
+        )
+        self.transform = transforms.Compose([transforms.ToPILImage(), transforms.ToTensor()])
+
+        # load configs and set random seed
+        self.configs = json.load(open(os.path.join(FACE_ROOT_DIR, "configs", "fer2013_config.json")))
+        self.image_size = (self.configs["image_size"], self.configs["image_size"])
+
+        self.model = resmasking_dropout1(in_channels=3, num_classes=7)
+        self.model.cuda()
+        self.state = torch.load(
+            os.path.join(FACE_ROOT_DIR, "Z_resmasking_dropout1_rot30_2019Nov30_13.32")
+        )
+        self.model.load_state_dict(self.state["net"])
+        self.model.eval()
+
+        self.count = 0
+
+    def __call__(self, frame: np.ndarray) -> np.ndarray:
+        return self.process_frame(frame)
+
+    def process_frame(self, frame: np.ndarray) -> np.ndarray:
+        out_frame = np.zeros_like(frame)
+
+        # frame = np.fliplr(frame)
+        frame = frame.astype(np.uint8)
+        h, w = frame.shape[:2]
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        blob = cv2.dnn.blobFromImage(
+            cv2.resize(frame, (300, 300)),
+            1.0,
+            (300, 300),
+            (104.0, 177.0, 123.0),
+        )
+        self.net.setInput(blob)
+        faces = self.net.forward()
+
+        for i in range(0, faces.shape[2]):
+            confidence = faces[0, 0, i, 2]
+            if confidence < 0.5:
+                continue
+            box = faces[0, 0, i, 3:7] * np.array([w, h, w, h])
+            start_x, start_y, end_x, end_y = box.astype("int")
+
+            # convert to square images
+            center_x, center_y = (start_x + end_x) // 2, (start_y + end_y) // 2
+            square_length = ((end_x - start_x) + (end_y - start_y)) // 2 // 2
+
+            square_length *= 1.1
+
+            start_x = int(center_x - square_length)
+            start_y = int(center_y - square_length)
+            end_x = int(center_x + square_length)
+            end_y = int(center_y + square_length)
+
+            cv2.rectangle(
+                out_frame, (start_x, start_y), (end_x, end_y), (179, 255, 179), 2
+            )
+
+            face = gray[start_y:end_y, start_x:end_x]
+
+            face = ensure_color(face)
+
+            face = cv2.resize(face, self.image_size)
+            face = self.transform(face).cuda()
+            face = torch.unsqueeze(face, dim=0)
+
+            output = torch.squeeze(self.model(face), 0)
+            proba = torch.softmax(output, 0)
+
+            # emo_idx = torch.argmax(proba, dim=0).item()
+            emo_proba, emo_idx = torch.max(proba, dim=0)
+            emo_idx = emo_idx.item()
+            emo_proba = emo_proba.item()
+
+            emo_label = FER_2013_EMO_DICT[emo_idx]
+
+            label_size, base_line = cv2.getTextSize(
+                "{}: 000".format(emo_label), cv2.FONT_HERSHEY_SIMPLEX, 0.8, 2
+            )
+
+            cv2.rectangle(
+                out_frame,
+                (end_x, start_y + 1 - label_size[1]),
+                (end_x + label_size[0], start_y + 1 + base_line),
+                (223, 128, 255),
+                cv2.FILLED,
+            )
+            cv2.putText(
+                out_frame,
+                "{} {}".format(emo_label, int(emo_proba * 100)),
+                (end_x, start_y + 1),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.8,
+                (0, 0, 0),
+                2,
+            )
+
+        self.count += 1
+        return out_frame
+
+
 def ensure_color(image):
     if len(image.shape) == 2:
         return np.dstack([image] * 3)
@@ -15,13 +122,6 @@ def ensure_color(image):
         return np.dstack([image] * 3)
     return image
 
-
-net = cv2.dnn.readNetFromCaffe(
-    os.path.join(FACE_ROOT_DIR, "deploy.prototxt.txt"),
-    os.path.join(FACE_ROOT_DIR, "res10_300x300_ssd_iter_140000.caffemodel")
-)
-
-transform = transforms.Compose([transforms.ToPILImage(), transforms.ToTensor()])
 
 FER_2013_EMO_DICT = {
     0: "angry",
@@ -35,22 +135,6 @@ FER_2013_EMO_DICT = {
 
 
 def main():
-    # load configs and set random seed
-    configs = json.load(open(os.path.join(FACE_ROOT_DIR, "configs", "fer2013_config.json")))
-    image_size = (configs["image_size"], configs["image_size"])
-
-    # model = densenet121(in_channels=3, num_classes=7)
-    model = resmasking_dropout1(in_channels=3, num_classes=7)
-    model.cuda()
-
-    # state = torch.load('./saved/checkpoints/densenet121_rot30_2019Nov11_14.23')
-    # state = torch.load('./saved/checkpoints/resmasking_dropout1_rot30_2019Nov17_14.33')
-    state = torch.load(
-        os.path.join(FACE_ROOT_DIR, "Z_resmasking_dropout1_rot30_2019Nov30_13.32")
-    )
-    model.load_state_dict(state["net"])
-    model.eval()
-
     # vid = cv2.VideoCapture(0)
     vid = cv2.VideoCapture("test_images/1.mp4")
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
@@ -65,95 +149,15 @@ def main():
     # cv2.namedWindow('disp')
     # cv2.resizeWindow('disp', width=800)
 
-    count = 0
+    estimator = FaceEmotionEstimator()
     with torch.no_grad():
         while True:
             success, frame = vid.read()
             if frame is None or success is not True:
                 break
 
-            frame = np.fliplr(frame).astype(np.uint8)
-            # frame += 50
-            h, w = frame.shape[:2]
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            # gray = frame
-
-            blob = cv2.dnn.blobFromImage(
-                cv2.resize(frame, (300, 300)),
-                1.0,
-                (300, 300),
-                (104.0, 177.0, 123.0),
-            )
-            net.setInput(blob)
-            faces = net.forward()
-
-            for i in range(0, faces.shape[2]):
-                confidence = faces[0, 0, i, 2]
-                if confidence < 0.5:
-                    continue
-                box = faces[0, 0, i, 3:7] * np.array([w, h, w, h])
-                start_x, start_y, end_x, end_y = box.astype("int")
-
-                # convert to square images
-                center_x, center_y = (start_x + end_x) // 2, (start_y + end_y) // 2
-                square_length = ((end_x - start_x) + (end_y - start_y)) // 2 // 2
-
-                square_length *= 1.1
-
-                start_x = int(center_x - square_length)
-                start_y = int(center_y - square_length)
-                end_x = int(center_x + square_length)
-                end_y = int(center_y + square_length)
-
-                cv2.rectangle(
-                    frame, (start_x, start_y), (end_x, end_y), (179, 255, 179), 2
-                )
-                # cv2.rectangle(frame , (x, y), (x + w, y + h), (179, 255, 179), 2)
-
-                # face = gray[y:y + h, x:x + w]
-                face = gray[start_y:end_y, start_x:end_x]
-
-                face = ensure_color(face)
-
-                face = cv2.resize(face, image_size)
-                face = transform(face).cuda()
-                face = torch.unsqueeze(face, dim=0)
-
-                output = torch.squeeze(model(face), 0)
-                proba = torch.softmax(output, 0)
-
-                # emo_idx = torch.argmax(proba, dim=0).item()
-                emo_proba, emo_idx = torch.max(proba, dim=0)
-                emo_idx = emo_idx.item()
-                emo_proba = emo_proba.item()
-
-                emo_label = FER_2013_EMO_DICT[emo_idx]
-
-                label_size, base_line = cv2.getTextSize(
-                    "{}: 000".format(emo_label), cv2.FONT_HERSHEY_SIMPLEX, 0.8, 2
-                )
-
-                cv2.rectangle(
-                    frame,
-                    (end_x, start_y + 1 - label_size[1]),
-                    (end_x + label_size[0], start_y + 1 + base_line),
-                    (223, 128, 255),
-                    cv2.FILLED,
-                )
-                cv2.putText(
-                    frame,
-                    "{} {}".format(emo_label, int(emo_proba * 100)),
-                    (end_x, start_y + 1),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    0.8,
-                    (0, 0, 0),
-                    2,
-                )
-
-            count += 1
+            frame = estimator(frame)
             out_vid.write(frame)
-
-            print(f'frame: {count}')
 
             # cv2.imshow("disp", frame)
             # cv2.imshow('disp', np.concatenate((gray ), axis=1))
